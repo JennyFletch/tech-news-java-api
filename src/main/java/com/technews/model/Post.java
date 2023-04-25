802982fad5af440e962313bd71db7a52ebbe40d5
JennyFletch <111204164+JennyFletch@users.noreply.github.com>
2023-04-25 16:47:13 -0600

    Models created and no errors found by IDE

---
+++ src/main/java/com/technews/model/Post.java
@@ -0,0 +1,131 @@
+package com.technews.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@Table(name = "post")
+public class Post implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String postUrl;
+    @Transient
+    private String userName;
+    @Transient
+    private int voteCount;
+    private Integer userId;
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "posted_at")
+    private Date postedAt = new Date();
+
+    @NotNull
+    @Temporal(TemporalType.DATE)
+    @Column(name = "updated_at")
+    private Date updatedAt = new Date();
+
+    @OneToMany(mappedBy = "postId", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Comment> comments;
+
+    public Post () {
+    }
+     public Post (Integer id, String title, String postUrl, int voteCount, Integer userId) {
+        this.id = id;
+        this.title = title;
+        this.postUrl = postUrl;
+        this.voteCount = voteCount;
+        this.userId = userId;
+     }
+
+     public Integer getId () {
+        return id;
+     }
+
+     public void setId (Integer id) {
+        this.id = id;
+     }
+     public String getTitle () {
+        return title;
+     }
+     public void setTitle (String title) {
+        this.title = title;
+     }
+     public String getPostUrl( ) {
+        return postUrl;
+     }
+     public void setPostUrl (String postUrl) {
+        this.postUrl = postUrl;
+     }
+     public String getUserName () {
+        return userName;
+     }
+     public void setUserName (String userName) {
+        this.userName = userName;
+     }
+     public int getVoteCount () {
+        return voteCount;
+     }
+     public void setVoteCount (int voteCount) {
+        this.voteCount = voteCount;
+     }
+     public Integer getUserId () {
+        return userId;
+     }
+     public void setUserId (Integer userId) {
+        this.userId = userId;
+     }
+     public Date getPostedAt () {
+        return postedAt;
+     }
+     public void setPostedAt () {
+        this.postedAt = postedAt;
+     }
+     public Date getUpdatedAt () {
+        return updatedAt;
+     }
+     public void setUpdatedAt (Date updatedAt) {
+        this.updatedAt = updatedAt;
+     }
+     public List<Comment> getComments () {
+        return comments;
+     }
+     public void setComments (List<Comment> comments) {
+        this.comments = comments;
+     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return voteCount == post.voteCount && Objects.equals(id, post.id) && Objects.equals(userName, post.userName) && Objects.equals(postedAt, post.postedAt) && Objects.equals(updatedAt, post.updatedAt) && Objects.equals(comments, post.comments);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userName, voteCount, postedAt, updatedAt, comments);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", voteCount=" + voteCount +
+                ", postedAt=" + postedAt +
+                ", updatedAt=" + updatedAt +
+                ", comments=" + comments +
+                '}';
+    }
+}
